ff8b4a827553462040018664584d2b5d96a62000
Damien Engels <engelsdamien@gmail.com>
2015-10-17 21:46:43 +0200

    Override reporter

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -4,11 +4,20 @@ import scala.scalajs.js
 import org.scalajs.dom
 import org.scalameter.api._
 import org.scalameter.picklers.noPickler._
+import org.scalameter.utils.Tree
+import org.scalameter.CurveData
 
 /**
  * @author dengels
  */
 object Main extends js.JSApp {
+
+  def echo(message: String): Unit = {
+    val paragraph = dom.document.createElement("p")
+    paragraph.innerHTML = "<strong>" + message + "</strong>"
+    dom.document.getElementById("playground").appendChild(paragraph)
+    println(message)
+  }
 
   object RangeBenchmark extends Bench.OfflineReport {
 
@@ -29,8 +38,36 @@ object Main extends js.JSApp {
             }
           }
         }
-    
-    override def reporter = Reporter.None
+
+    override def reporter = new Reporter[Double] {
+      def report(result: CurveData[Double], persistor: Persistor): Unit = {
+        println(s"\nOne Test finished : success = ${result.success}")
+
+        println("\nContext:")
+        val ctx = result.context
+        ctx.properties.foreach { x => println(s"${x._1} -> ${x._2}") }
+
+        println("\nInfo:")
+        val info = result.info
+        info.foreach { x => println(s"${x._1} -> ${x._2}") }
+
+        println("\nMeasurements:")
+        val measurements = result.measurements
+        measurements.foreach { x => 
+          val axisData = x.params.toString
+          val result = x.value + x.units
+          echo(s"$axisData : $result")
+          }
+      }
+
+      def report(results: Tree[CurveData[Double]], persistor: Persistor): Boolean = {
+        println("\n\nAll Test finished : ")
+
+        true
+      }
+
+    }
+
     override def executor = LocalExecutor(warmer, aggregator, measurer)
     override def persistor = Persistor.None
   }
